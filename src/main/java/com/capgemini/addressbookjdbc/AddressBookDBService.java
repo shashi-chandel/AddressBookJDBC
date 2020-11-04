b980c39da95b52cc6812582bde73f5b9de602c8a
Your Name <you@example.com>
2020-11-04 21:34:40 +0530

    [shashi] Added UC16 : Test case and method added to retrieve data from DB

--- src/main/java/com/capgemini/addressbookjdbc/AddressBookDBService.java
+++ src/main/java/com/capgemini/addressbookjdbc/AddressBookDBService.java
@@ -10,6 +10,56 @@ import java.util.List;
 
 public class AddressBookDBService {
 
+	private static AddressBookDBService addressBookDBService;
+
+	private AddressBookDBService() {
+	}
+
+	public static AddressBookDBService getInstance() {
+		if (addressBookDBService == null)
+			addressBookDBService = new AddressBookDBService();
+		return addressBookDBService;
+	}
+
+	public List<Contact> readData() {
+		String sql = "SELECT * from address_book;";
+		return this.getContactDetailsUsingSqlQuery(sql);
+	}
+
+	private List<Contact> getContactDetailsUsingSqlQuery(String sql) {
+		List<Contact> ContactList = null;
+		try (Connection connection = addressBookDBService.getConnection();) {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			ResultSet result = preparedStatement.executeQuery(sql);
+			ContactList = this.getAddressBookData(result);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ContactList;
+	}
+
+	private List<Contact> getAddressBookData(ResultSet resultSet) {
+		List<Contact> contactList = new ArrayList<>();
+		try {
+			while (resultSet.next()) {
+				String firstName = resultSet.getString("first_Name");
+				String lastName = resultSet.getString("last_Name");
+				String address = resultSet.getString("address");
+				String city = resultSet.getString("city");
+				String state = resultSet.getString("state");
+				int zip = resultSet.getInt("zip");
+				String phoneNumber = resultSet.getString("phone");
+				String email = resultSet.getString("email");
+				String addressBookType = resultSet.getString("type");
+				contactList.add(new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email,
+						addressBookType));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+
 	private Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service?useSSL=false";
 		String userName = "root";
--- src/test/java/com/capgemini/addressbookjdbc/AddressBookServiceTest.java
+++ src/test/java/com/capgemini/addressbookjdbc/AddressBookServiceTest.java
@@ -6,5 +6,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AddressBookServiceTest {
-	
+	@Test
+	public void contactsWhenRetrievedFromDB_ShouldMatchCount() {
+		AddressBookService addressBookService = new AddressBookService();
+		List<Contact> contactList = addressBookService.readContactData();
+		Assert.assertEquals(7, contactList.size());
+	}
 }
